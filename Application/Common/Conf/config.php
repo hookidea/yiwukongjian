999906225ea9c2f39ab2cbab82c406f66e3ce09a
hookidea <hookidea@gmail.com>
2016-06-02 07:43:45 +0800

    初次提交

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -6,7 +6,7 @@ return array(
     'DB_HOST'     => '127.0.0.1',// 服务器地址
     'DB_NAME'     => 'shop',     // 数据库名
     'DB_USER'     => 'root',     // 用户名
-    'DB_PWD'      => '*****',    // 密码
+    'DB_PWD'      => '',    // 密码
     'DB_PORT'     => 3306,       // 端口
     'DB_PARAMS'   => array(),    // 数据库连接参数
     'DB_PREFIX'   => '',         // 数据库表前缀
